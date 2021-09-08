46926150fb03d129ec49d5f0b80cbde223431851
Ренат <safych@MacBook-Pro.local>
2021-09-08 21:47:50 +0300

    add tasks images

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,14 @@ const uglifyEs = require('gulp-uglify-es').default;
 // html
 const fileinclude = require('gulp-rigger');
 
+// images
+const imagemin = require('gulp-imagemin');
+const spritesmith = require('gulp.spritesmith');
+const svgSprite = require('gulp-svg-sprite');
+const svgmin = require('gulp-svgmin');
+const cheerio = require('gulp-cheerio');
+const replace = require('gulp-replace');
+
 // clean
 const clean = () => {
     return del(['build']);
@@ -27,7 +35,7 @@ const clean = () => {
 
 // scss
 const stylesBuild = () => {
-    return src('./dev/static/sass/*.{scss,sass}')
+    return src('dev/static/sass/*.{scss,sass}')
         .pipe(sass({
             fiber: Fiber, // оптимизация компиляции (прирост скорости примерно 10-15%)
         }))
@@ -48,7 +56,7 @@ const stylesBuild = () => {
 }
 
 const stylesDev = () => {
-    return src('./dev/static/sass/*.{scss,sass}')
+    return src('dev/static/sass/*.{scss,sass}')
         .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(sass({
@@ -93,7 +101,7 @@ const scriptDev = () => {
 }
 
 // html
-const htmlDev = () => {
+const html = () => {
     return src('dev/*.html') // ['dev/**/*.html', '!dev/blocks/*.html']
         .pipe(plumber())
         .pipe(fileinclude())
@@ -107,8 +115,100 @@ const htmlDev = () => {
 // fonts
 const fonts = () => {
     return src('dev/static/fonts/**/*')
-      .pipe(dest('build/static/fonts/'))
+        .pipe(dest('build/static/fonts/'))
 }
 
-exports.default = series(clean, parallel(stylesDev, scriptDev, htmlDev, fonts));
-exports.build = series(clean, parallel(stylesBuild, scriptBuild, fonts));
+// images
+const imagesDev = () => {
+    return src('dev/static/img/**/*.{gif,png,jpg,svg}')
+        .pipe(dest('build/static/img/'));
+}
+
+const imagesBuild = () => {
+    return src([
+        'dev/static/img/**/*.{gif,png,jpg,svg}',
+        '!dev/static/img/icon-png/*.png',
+        '!dev/static/img/svg/*.svg'
+    ])
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.mozjpeg({quality: 85, progressive: true}),
+            imagemin.optipng({optimizationLevel: 3}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        .pipe(dest('build/static/img/'));
+}
+
+const createSpritePng = () => {
+    const spriteData = src('dev/static/img/icon-png/*.png')
+    .pipe(spritesmith({
+      imgName: 'sprite.png',
+      cssName: '_sprite.scss',
+      padding: 10
+    }));
+    spriteData.css.pipe(dest('dev/static/sass/utils'));
+    return spriteData.img.pipe(dest('build/static/img/'));
+}
+
+const createSpriteSvgColor = () => {
+    return src('dev/static/img/svg/*.svg')
+	    // minify svg
+		.pipe(svgmin({
+			js2svg: {
+				pretty: true
+			}
+		}))
+		// cheerio plugin create unnecessary string '&gt;', so replace it.
+		.pipe(replace('&gt;', '>'))
+		// build svg sprite
+		.pipe(svgSprite({
+			mode: {
+				symbol: {
+					sprite: "sprite.svg",
+				}
+			}
+		}))
+		.pipe(dest('build/static/img'));
+}
+
+const createSpriteSvgBlack = () => {
+    return src('dev/static/img/svg/*.svg')
+	    // minify svg
+		.pipe(svgmin({
+			js2svg: {
+				pretty: true
+			}
+		}))
+		// remove all fill, style and stroke declarations in out shapes
+		.pipe(cheerio({
+			run: function ($) {
+				$('[fill]').removeAttr('fill');
+				// $('[stroke]').removeAttr('stroke');
+				$('[style]').removeAttr('style');
+			},
+			parserOptions: {xmlMode: true}
+		}))
+		// cheerio plugin create unnecessary string '&gt;', so replace it.
+		.pipe(replace('&gt;', '>'))
+		// build svg sprite
+		.pipe(svgSprite({
+			mode: {
+				symbol: {
+					sprite: "sprite.svg",
+				}
+			}
+		}))
+		.pipe(dest('build/static/img'));
+}
+
+exports.default = series(clean, parallel(stylesDev, scriptDev, html, fonts, imagesDev));
+exports.build = series(clean, parallel(stylesBuild, scriptBuild, html, fonts, imagesBuild));
+
+exports.spritepng = createSpritePng;
+exports.spritesvgcolor = createSpriteSvgColor;
+exports.spritesvgblack = createSpriteSvgBlack;
